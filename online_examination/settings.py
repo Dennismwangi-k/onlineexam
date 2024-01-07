4bf810c2c0066d123d8b53b8b8c871ec381bde58
Weshy Joe <josephwaweru96@gmail.com>
2024-01-07 19:50:39 +0300

    setup and addition of gitignore

--- online_examination/settings.py
+++ online_examination/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+8r)%%jvgwm_mh_o16m)w(m#4&476u**hkpd1hifkm$mkkaad1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'exam',
 ]
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
